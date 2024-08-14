126882ad94e297833e6d387b2b0d05ff69b13447
Yemi Alade <ylawal7@gmail.com>
2024-08-14 13:21:59 +0000

    Add functionality to start game by flipping a card

--- script.js
+++ script.js
@@ -4,17 +4,42 @@ let matches = 0;
 let attempts = 0;
 let timeLeft = 60;
 let timerInterval;
+let gameStarted = false;
 
 // Code to shuffle cards
-
 function shuffleCards() {
     let gameBoard = document.getElementById('game-board');
     let cardElements = [...gameBoard.children];
-    for (let card of cards) {
-        card.style.order = Math.floor(Math.random() * cardElements);
+    for (let card of cardElements) {
+        card.style.order = Math.floor(Math.random() * cardElements.length);
     }
 };
 
 function initialiseGame() {
-    cards = 
+    cards = [...document.getElementsByClassName('card')];
+    for (let card of cards) {
+        card.addEventListener('click', flipCard);
+    }
+    shuffleCards ();
+}
+
+function flipCard() {
+    if (!gameStarted) {
+        startTimer();
+        gameStarted = true;
+    }
+
+    if (flippedCards.length < 2 && !flippedCards.includes(this) && !this.classList.contains('matched')) {
+        this.classList.add('flipped');
+        this.style.backgroundColor = this.dataset.color;
+        this.querySelector('svg').style.fill = 'white';
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) {
+            attempts++;
+            document.getElementById('attempts').textContent = attempts;
+            setTimeout(checkMatch, 1000);
+        }
+    }
+
 }

70b4ceee2eca0b94bcf07fd2ab3d5c39dc54d68c
Yemi Alade <ylawal7@gmail.com>
2024-08-19 14:23:10 +0000

    Fix function to display cards on preview

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const resetButton = document.getElementById('reset-button');
 
 let cards = [];
 let flippedCards = [];
-let matchesCounter = 0;
-let attemptsCounter = 0;
+let matches = 0;
+let attempts = 0;
 let timeLeft = 60;
 let gameInterval;
 
@@ -29,51 +29,52 @@ let gameCards = [...cardData, ...cardData];
 
 document.addEventListener('DOMContentLoaded', () => {
     initialiseGame();
-    card = [...document.getElementsByClassName('card')];
 });
 
 // Function to create cards
 function createCard(card, index) {
     const cardElement = document.createElement('div');
     cardElement.classList.add('card');
-    cardElement.dataset.index('index');
+    // cardElement.dataset.index('index');
+    cardElement.dataset.index = index;
     cardElement.innerHTML = `
         <div class = 'card card-inner'>
-            <div class = 'card-front'>
-                <img src='${card.image}' alt = 'Card Front'>Card with image</img>
+            <div class = 'card-front'> Card with no image
+                <img src='assets/images/card-back.png' alt='Card Back'>
             </div>
 
-            <div class = 'card-back'>
-                <img src='assets/images/card-back.png' alt = 'Card Back'>Card with no image</img>
+            <div class = 'card-back'> Card with image
+                <img src='${card.image}' alt='Card Front'>
             </div>
         </div>
         `;
-    cardElement.addEventListener('click', flipCard);
+    // cardElement.addEventListener('click', flipCard);
     return cardElement;
 }
 
 // Function to shuffle cards
-function shuffleCards(gameCards) {
-    for (let i = array.length - 1; i > 0; i--) {
+function shuffleCards(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [cards[i], cards[j]] = [cards[j], cards[i]];
       }
-      return array;
+      return cards;
 };
 
 // Function to initialise game
 function initialiseGame() {
-  shuffleCards(gameCards);
+  cards = shuffleCards([...gameCards]);
   gameBoard.innerHTML = '';
+  console.log("cards array in initialize game = ", cards)
   cards.forEach((card, index) => {
     gameBoard.appendChild(createCard(card, index));
   });
 }
 
-function turnCard() { 
-    let box = document.getElementsByClassName('card'); 
-    box.addEventListener('click', flipCard); 
-} 
+// function turnCard() { 
+//     let box = document.getElementsByClassName('card'); 
+//     box.addEventListener('click', flipCard); 
+// } 
 
 // function changeCard() { 
      

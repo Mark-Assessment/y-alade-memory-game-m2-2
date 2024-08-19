6929ea93b7eda87e3e1f43cfbbc8f0635f93a89e
Yemi Alade <ylawal7@gmail.com>
2024-08-19 13:33:58 +0000

    Adjust function to create cards to see cards in preview

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const resetButton = document.getElementById('reset-button');
 
 let cards = [];
 let flippedCards = [];
-let matches = 0;
-let attempts = 0;
+let matchesCounter = 0;
+let attemptsCounter = 0;
 let timeLeft = 60;
 let gameInterval;
 
@@ -30,8 +30,6 @@ let gameCards = [...cardData, ...cardData];
 document.addEventListener('DOMContentLoaded', () => {
     initialiseGame();
     card = [...document.getElementsByClassName('card')];
-    card.style.backgroundColor('white');
-
 });
 
 // Function to create cards
@@ -40,13 +38,13 @@ function createCard(card, index) {
     cardElement.classList.add('card');
     cardElement.dataset.index('index');
     cardElement.innerHTML = `
-        <div class = 'card-inner'>
+        <div class = 'card card-inner'>
             <div class = 'card-front'>
                 <img src='${card.image}' alt = 'Card Front'>Card with image</img>
             </div>
 
             <div class = 'card-back'>
-                <img src='' alt = 'Card Back'>Card with no image</img>
+                <img src='assets/images/card-back.png' alt = 'Card Back'>Card with no image</img>
             </div>
         </div>
         `;
@@ -55,23 +53,21 @@ function createCard(card, index) {
 }
 
 // Function to shuffle cards
-function shuffleCards() {
-    let gameBoard = document.getElementById('game-board');
-    let cardElements = [...gameBoard.children];
-    for (let card of cardElements) {
-        card.style.order = Math.floor(Math.random() * cardElements.length);
-    }
+function shuffleCards(gameCards) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
 };
 
-// Initialise game
+// Function to initialise game
 function initialiseGame() {
-    shuffleCards ();
-    cards = [...document.getElementsByClassName('card')];
-    cards = document.getElementById('test1');
-    for (let card of cards) {
-        // card.style.backgroundColor.remove('white');
-        cards.addEventListener('click', flipCard);
-    }    
+  shuffleCards(gameCards);
+  gameBoard.innerHTML = '';
+  cards.forEach((card, index) => {
+    gameBoard.appendChild(createCard(card, index));
+  });
 }
 
 function turnCard() { 
@@ -83,76 +79,5 @@ function turnCard() {
      
 // };
 
-function flipCard() {
-    const cardChanger = document.getElementsByClassName('card'); 
-    cardChanger.classList.toggle("hide");
-
-    if (!gameStarted) {
-        startTimer();
-        gameStarted = true;
-    }
-
-    if (flippedCards.length < 2 && !flippedCards.includes(this) && !this.classList.contains('matched')) {
-        this.classList.add('flipped');
-        card.classList.remove('hide');
-        flippedCards.push(this);
-
-        if (flippedCards.length === 2) {
-            document.getElementById('attempts').textContent = attempts;
-            attempts++;
-            setTimeout(checkMatch, 1000);
-        }
-    }
-};
-
-//Check if cards match
-function checkMatch() {
-    const [card1, card2] = flippedCards;
-    if (card1.dataset.image === card2.dataset.image) {
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-        matches++;
-        document.getElementById('matches').textContent = matches;
-        if (matches === cards.length / 2) {
-            alert('You have made a match!')
-            // endGame(true);
-        }
-    } else {
-        card1.classList.remove('flipped');
-        card2.classList.remove('flipped');
-    }
-    flippedCards = [];
-}
-
-
-document.getElementById('reset-button').addEventListener('click', resetGame);
-
-// Function to restart the game
-function resetGame() {
-    document.getElementById('matches').textContent = matches;
-    document.getElementById('attempts').textContent = attempts;
-    document.getElementById('time').textContent = timeLeft;
-    matches = 0;
-    attempts = 0;
-    timeLeft = 60;
-    gameStarted = false;
-    clearInterval(timerInterval);
-    for (const card of cards) {
-        card.classList.remove('matched', 'flipped');
-    }
-    shuffleCards();
-}
-
-
-function startTimer() {
-    clearInterval(timerInterval);
-    timerInterval = setInterval(() => {
-        timeLeft--;
-        document.getElementById('time').textContent = timeLeft;
-        if (timeLeft <= 0) {
-            endGame(false);
-        }
-    }, 1000);
-}
 
 

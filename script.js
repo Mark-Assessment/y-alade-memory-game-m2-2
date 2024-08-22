d2d2aa1d41029aeab3234fa9e5e6fea17041bfba
Yemi Alade <ylawal7@gmail.com>
2024-08-22 13:38:51 +0000

    Update functions to check for match

--- script.js
+++ script.js
@@ -6,21 +6,24 @@ const resetButton = document.getElementById('reset-button');
 
 
 let cards = [];
+let firstCard = '';
+let secondCard = '';
 let flippedCards = [];
 let matches = 0;
 let attempts = 0;
 let timeLeft = 90;
 let gameInterval;
+let canFlip = true;
 
 const cardData = [
-    {image: 'assets/images/circle.png'},
-    {image: 'assets/images/diamond.png'},
-    {image: 'assets/images/heart.png'},
-    {image: 'assets/images/hexagon.png'},
-    {image: 'assets/images/pentagon.png'},
-    {image: 'assets/images/square.png'},
-    {image: 'assets/images/star.png'},
-    {image: 'assets/images/triangle.png'},
+    {image: 'assets/images/circle.png', name: 'circle'},
+    {image: 'assets/images/diamond.png', name: 'diamond'},
+    {image: 'assets/images/heart.png', name: 'heart'},
+    {image: 'assets/images/hexagon.png', name:'hexagon'},
+    {image: 'assets/images/pentagon.png', name: 'pentagon'},
+    {image: 'assets/images/square.png', name: 'square'},
+    {image: 'assets/images/star.png', name: 'star'},
+    {image: 'assets/images/triangle.png', name: 'triangle'},
 ];
 
 // Create copies of card data to create pairs
@@ -35,19 +38,17 @@ document.addEventListener('DOMContentLoaded', () => {
 function createCard(card, index) {
     const cardElement = document.createElement('div');
     cardElement.classList.add('card');
-    // cardElement.dataset.index('index');
     cardElement.dataset.index = index;
     cardElement.innerHTML = `
-        <div class = 'card card-inner'>
-            <div class = 'card-front'>
-                <img src='assets/images/card-back.png' alt='Card Back'>
+        <div class="card-inner">
+            <div class="card-front">
+                <img src="assets/images/card-back.png" alt="Card back">
             </div>
-
-            <div class = 'card-back'>
-                <img src='${card.image}' alt='Card Front'>
+            <div class="card-back">
+                <img src="${card.image}" alt="${card.name} shape">
             </div>
         </div>
-        `;
+    `;
     cardElement.addEventListener('click', flipCard);
     return cardElement;
 }
@@ -65,28 +66,72 @@ function shuffleCards(cards) {
 function initialiseGame() {
   cards = shuffleCards([...gameCards]);
   gameBoard.innerHTML = '';
-  console.log("cards array in initialize game = ", cards)
   cards.forEach((card, index) => {
     gameBoard.appendChild(createCard(card, index));
   });
 }
 
 // Function to turn card
-function flipCard() {    
+function flipCard() {  
+    
+    if (!canFlip || this.classList.contains('flipped') || this === firstCard) return;
 
-    if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
-        this.classList.add('flipped');
-        flippedCards.push(this);
-    };
+    this.classList.add('flipped');
+
+    if (!firstCard) {
+        firstCard = this;
+    } else {
+        secondCard = this;
+        canFlip = false;
+        checkForMatch();
+    }
+}
+
+    function checkForMatch() {
+        attempts++;
+        attemptsCounter.textContent = attempts;
+    
+        const isMatch = cards[parseInt(firstCard.dataset.index)].name === cards[parseInt(secondCard.dataset.index)].name;
+    
+        if (isMatch) {
+            matches++;
+            matchesCounter.textContent = matches;
+            resetCards();
+            if (matches === cardData.length) {
+                endGame(true);
+            }
+        } else {
+            setTimeout(unflipCards, 1000);
+        }
+    }
+    
+    function unflipCards() {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        resetCards();
+    }
+    
+    function resetCards() {
+        [firstCard, secondCard] = [null, null];
+        canFlip = true;
+    }
+
 
     // Function start timer with first card flip
     function startTimer () {
         gameInterval = setInterval(() => {
-            timeLeft--;}
-    )
-    }
+            timeLeft--;
+            timer.textContent = timeLeft;
+        if (timeLeft === 0) {
+            endGame(false);
+        }
+    }, 1000);
+}
+        
 
-};
+
+
+
 
 
 
